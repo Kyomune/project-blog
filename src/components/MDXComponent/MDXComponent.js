75fa0c43919c132c36e2d656e8af0e962f709a62
Breyner Sánchez <breinersanchez97@hotmail.com>
2024-04-06 16:34:56 -0400

    exercise 4

--- src/components/MDXComponent/MDXComponent.js
+++ src/components/MDXComponent/MDXComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 import { MDXRemote } from "next-mdx-remote/rsc";
+import CodeSnippet from "../CodeSnippet";
 
 function MDXComponent({ content }) {
-  return <MDXRemote source={content} />;
+  return (
+    <MDXRemote
+      source={content}
+      components={{
+        pre: CodeSnippet,
+      }}
+    />
+  );
 }
 
 export default MDXComponent;
